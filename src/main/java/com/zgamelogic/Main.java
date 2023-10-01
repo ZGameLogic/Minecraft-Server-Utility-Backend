fd772a9d2e363881e602224d46fe48bec74d5629
BShabowski <benshabowski@gmail.com>
2023-10-01 13:21:27 -0500

    CORS am I right?

--- src/main/java/com/zgamelogic/Main.java
+++ src/main/java/com/zgamelogic/Main.java
@@ -2,7 +2,10 @@ package com.zgamelogic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,5 +13,15 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Main.class);
         app.run();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
