7194c2c47e69ca34cc85215dabdceb4a3d518562
BShabowski <benshabowski@gmail.com>
2023-12-12 10:50:10 -0600

    Fixing logs

--- src/main/java/com/zgamelogic/data/database/user/UserRepository.java
+++ src/main/java/com/zgamelogic/data/database/user/UserRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END " +
-            "FROM User u JOIN u.permissions p " +
-            "WHERE u.id = :userId AND KEY(p) = :server AND VALUE(p) LIKE %:permission%")
+    @Query("""
+            SELECT CASE WHEN COUNT(u) > 0
+            THEN true ELSE false END
+            FROM User u JOIN u.permissions p WHERE
+            (u.id = :userId AND KEY(p) = 'General Permissions' AND VALUE(p) LIKE '%A%') OR
+            (u.id = :userId AND KEY(p) = :server AND VALUE(p) LIKE %:permission%)
+            """)
     boolean userHasPermission(@Param("userId") String userId, @Param("server") String server, @Param("permission") String permission);
 }
